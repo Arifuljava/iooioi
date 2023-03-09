a9a054406d47b9a72f149f14573f0402fc4e1ce4
Arifuljava <arifulislam.java.c@gmail.com>
2023-03-09 14:48:28 +0600

    Second@

---
+++ app/src/main/java/com/grozziie/testingApplications/CPClActivity.java
@@ -0,0 +1,13 @@
+package com.grozziie.testingApplications;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CPClActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_p_cl);
+    }
+}
